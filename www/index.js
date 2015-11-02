d02d8c4c6dd8b7669a38f3d9ab6c0cf7d7d9c70f
Alex Ewerlöf <hanifbbz@gmail.com>
2015-11-02 19:20:28 +0100

    the primitive version of the slideshow now works

--- www/index.js
+++ www/index.js
@@ -1,8 +1,20 @@
 angular.module('app', [])
-.controller('IndexController', function ($scope, $http) {
-  $scope.foo = 'bars';
-  $http.get('/api/slides')
-  .success(function (slides) {
-    $scope.foo = slides;
+.controller('IndexController', function ($scope, $http, $timeout) {
+  $http.get('/api/slides').success(function (slides) {
+    $scope.slides = slides;
+    changeSlide();
   });
+
+  var INTERVAL = 1000;
+  var index = 0;
+  $scope.getCurrentSlide = function () {
+    return $scope.slides ? 'url(slides/' + $scope.slides[index] + ')' : '';
+  }
+  function changeSlide() {
+    index++;
+    if (index >= $scope.slides.length) {
+      index = 0;
+    }
+    $timeout(changeSlide, INTERVAL);
+  }
 });
