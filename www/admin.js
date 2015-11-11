20ce30afa511e08ef9fc4e7d023025a7ba3f4810
Alex Ewerlöf <hanifbbz@gmail.com>
2015-11-11 18:48:51 +0100

    created a very basic admin page

--- www/admin.js
+++ www/admin.js
@@ -1,1 +1,19 @@
-alert('nope');
+angular.module('admin', [])
+.controller('AdminController', function ($scope, $http) {
+	$scope.db = {
+		slides: [
+			{
+				url: 'slides/1.png',
+				interval: 1
+			},
+			{
+				url: 'slides/2.jpg',
+				interval: 3
+			},
+			{
+				url: 'slides/3.jpg',
+				interval: 1
+			}
+		]
+	};
+});
