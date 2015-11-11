b664c181ecba15863fb359da035c2bdb32d16e87
Alex Ewerlöf <hanifbbz@gmail.com>
2015-11-11 19:02:30 +0100

    can read the json file

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 exports.read = function read(req, res) {
-	fs.read('./db.json', function (err, contents) {
+	fs.readFile('./db.json', function (err, contents) {
 		if (err) throw err;
 		res.send(JSON.parse(contents));
 	});
